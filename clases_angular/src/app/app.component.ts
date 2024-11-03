18f2425043a516019cbe8574e223878eb6f2ba2d
pedro <rositopedro@gmail.com>
2024-11-03 17:21:17 -0300

    Se añade codigo clase 3

--- clases_angular/src/app/app.component.ts
+++ clases_angular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreditCardComponent } from './credit-card/credit-card.component';
 import { ListadoDispositivosComponent } from './listado-dispositivos/listado-dispositivos.component';
+import { FormulariosComponent } from './formularios/formularios.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CreditCardComponent, ListadoDispositivosComponent],
+  imports: [RouterOutlet, CreditCardComponent, ListadoDispositivosComponent, FormulariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
