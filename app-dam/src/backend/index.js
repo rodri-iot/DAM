d024417b252276b0619562c17aea96f44eaed1b8
pedro <rositopedro@gmail.com>
2024-11-17 18:19:46 -0300

    Se añade proyecto docker y trabajo final

---
+++ app-dam/src/backend/index.js
@@ -0,0 +1,36 @@
+//=======[ Settings, Imports & Data ]==========================================
+
+var PORT    = 3000;
+
+const cors = require('cors');
+
+var express = require('express');
+var app = express();
+var pool = require('./mysql-connector');
+const routerDispositivo = require('./routes/dispositivo')
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+// to parse application/json
+app.use(express.json()); 
+// to serve static files
+app.use(express.static('/home/node/app/static/'));
+// to enable cors
+app.use(cors(corsOptions));
+
+app.use('/dispositivo', routerDispositivo)
+
+//=======[ Main module code ]==================================================
+
+app.get('/', function(req, res, next) {
+    res.send({'mensaje': 'Hola DAM'}).status(200);
+});
+
+app.listen(PORT, function(req, res) {
+    console.log("NodeJS API running correctly");
+});
+
+//=======[ End of file ]=======================================================
