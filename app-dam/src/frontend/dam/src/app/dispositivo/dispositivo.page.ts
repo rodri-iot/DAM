ecbfac32d0dd8f371561818c863eebee33b527ec
pedro <rositopedro@gmail.com>
2024-11-25 12:09:39 -0300

    Se añade contenido clase 6

--- app-dam/src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
+++ app-dam/src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Observable, Subscription, fromEvent, interval } from 'rxjs';
+import { DispositivoService } from '../services/dispositivo.service';
 
 @Component({
   selector: 'app-dispositivo',
@@ -10,11 +12,48 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class DispositivoPage implements OnInit {
+export class DispositivoPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  observable$: Observable<any>
+  dispositivos: any = []
+  // subscription: Subscription
 
-  ngOnInit() {
+  mouseMove$ = fromEvent(document, 'mousemove')
+
+  constructor(private _dispositivoService: DispositivoService) {
+    this.observable$ = interval(1000)
+
+    // this.subscription = this.mouseMove$.subscribe((evt: any) => {
+    //   console.log(`Coords: ${evt.clientX} X ${evt.clientY} Y`)
+    // })
+    // this.subscription = this.observable$.subscribe((value) => {
+    //   console.log(value)
+    // })
+  }
+
+  // subscribe () {
+  //   this.subscription = this.observable$.subscribe((value) => {
+  //     console.log(value)
+  //   })
+  // }
+
+  // unsubscribe () {
+  //   this.subscription.unsubscribe()
+  // }
+
+  ngOnInit () {
+    this._dispositivoService.getDispositivos()
+      .then((data) => {
+        this.dispositivos = data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    console.log(this.dispositivos)
+  }
+
+  ngOnDestroy () {
+    // this.subscription.unsubscribe()
   }
 
 }
